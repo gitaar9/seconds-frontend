be1e2882ab273938435d4fafb5d72e133b3a72db
gitaar9 <thijs.eker@gmail.com>
2023-05-18 23:27:54 +0200

    change ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiIp: 'http://157.230.20.115:8000',
+  apiIp: '157.230.20.115:8000',
   apiUrl: 'http://157.230.20.115:8000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: true,
-  apiIp: 'http://157.230.20.115:8000',
+  apiIp: '157.230.20.115:8000',
   apiUrl: 'http://157.230.20.115:8000'
 };
 
